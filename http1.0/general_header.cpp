da30105644450b8d66bc8b34b90cb0158f96258f
Sudhish Sewpaul <sudhishsewpaul@hotmail.com>
2025-02-04 12:25:48 -0800

    Separate http time from general header

--- http1.0/general_header.cpp
+++ http1.0/general_header.cpp
@@ -2,83 +2,39 @@
 #include <string>
 #include <chrono>
 #include <iostream>
+#include "http_time.cpp"
 
 static const std::string GENERAL_HEADER_FIELDS[2] = {"Date", "Pragma"};
 
 class GeneralHeader
 {
 public:
-    time_t date;
-    struct tm tm;
+    HTTPTime date;
     std::string pragma;
-    bool is_date_valid;
 
-    GeneralHeader()
-    {
-        auto t = std::chrono::system_clock::now();
-        date = std::chrono::system_clock::to_time_t(t);
+    GeneralHeader() {
+        date = HTTPTime();
         pragma = "";
-        is_date_valid = false;
     };
 
     GeneralHeader(std::string strdate, std::string reqpragma)
     {
-        const char *status = strptime(strdate.c_str(), "%a, %d %b %Y %H:%M:%S GMT", &tm);   // RFC 822 Format
-
-        if (status == NULL)
-        {
-            status = strptime(strdate.c_str(), "%a, %d-%b-%y %H:%M:%S GMT", &tm);   // RFC 850 Format
-
-            if (status == NULL)
-            {
-                status = strptime(strdate.c_str(), "%a %b %d %H:%M:%S %Y", &tm);    // ANSI C asctime() Format
-
-                if (status == NULL)
-                {
-                    std::cerr << "Cannot process time" << std::endl;
-                    return;
-                }
-            }
-        }
-
-        date = mktime(&tm);
+        date = HTTPTime(strdate);
         pragma = reqpragma;
-
-        is_date_valid = true;
     }
 
     int setValue(std::string field, std::string value)
     {
-
         if (field == "Date")
         {
-            if (is_date_valid)
+            if (date.valid)
             {
                 return -1;
             }
 
-            const char *status = strptime(value.c_str(), "%a, %d %b %Y %H:%M:%S GMT", &tm);     // RFC 822 Format
-
-            if (status == NULL)
-            {
-                status = strptime(value.c_str(), "%a, %d-%b-%y %H:%M:%S GMT", &tm);     // RFC 850 Format
-
-                if (status == NULL)
-                {
-                    status = strptime(value.c_str(), "%a %b %d %H:%M:%S %Y", &tm);      // ANSI C asctime() Format
-
-                    if (status == NULL)
-                    {
-                        std::cerr << "Cannot process time" << std::endl;
-                        return -1;
-                    }
-                }
+            if (date.update_time(value)) {
+                return 0;
             }
-
-            date = mktime(&tm);
-            is_date_valid = true;
-
-            return 0;
         }
         else if (field == "Pragma" && pragma.empty())
         {
