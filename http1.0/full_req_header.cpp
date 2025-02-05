3e4b42e48a9fd123e07986ccf6abc58e2f43848e
Sudhish Sewpaul <sudhishsewpaul@hotmail.com>
2025-02-05 02:51:27 -0800

    Handle request headers

--- http1.0/full_req_header.cpp
+++ http1.0/full_req_header.cpp
@@ -38,10 +38,11 @@ private:
         int added_status = -2;
         for (auto field : GENERAL_HEADER_FIELDS)
         {
-            std::string curr_value = line.substr(field.length() + 2);
             if (line.find(field + ":") != std::string::npos)
             {
-                if (num_general_headers > 0 && (added_status = general_headers[num_general_headers - 1].setValue(field, curr_value)) == 0) {
+                std::string curr_value = line.substr(field.length() + 2);
+                if (num_general_headers > 0 && (added_status = general_headers[num_general_headers - 1].setValue(field, curr_value)) == 0)
+                {
                     return true;
                 }
                 else if (num_general_headers == 0 || (num_general_headers > 0 && added_status == -1))
@@ -58,11 +59,43 @@ private:
         return false;
     }
 
+    bool check_and_process_request_headers(std::string &line)
+    {
+        bool added_status = false;
+        for (auto field : REQUEST_HEADER_FIELDS)
+        {
+            if (line.find(field + ":") != std::string::npos)
+            {
+                std::string curr_value = line.substr(field.length() + 2);
+                if (num_request_headers > 0 && (added_status = request_headers[num_request_headers - 1].update_request_header(field, curr_value)))
+                {
+                    return true;
+                }
+                else if (num_request_headers == 0 || (num_request_headers > 0 && !added_status))
+                {
+                    RequestHeader h;
+                    h.update_request_header(field, curr_value);
+                    num_request_headers += 1;
+                    request_headers.push_back(h);
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
     void process_headers(std::vector<std::string> header)
     {
         for (auto it = header.begin() + 1; it != header.end(); it++)
         {
-            if (check_and_process_general_headers(*it)) {
+            if (check_and_process_general_headers(*it))
+            {
+                continue;
+            }
+            
+            if (check_and_process_request_headers(*it))
+            {
                 continue;
             }
         }
